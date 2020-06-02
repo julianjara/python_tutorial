40a02a004863353b70ac741c066263cbaaf4ed61
Julian <name@mail.com>
2020-06-02 20:50:18 +1000

    capitulo4

---
+++ capitulo4/conditions.py
@@ -0,0 +1,17 @@
+# conditions statements
+print(5>2)
+print(5<2)
+print('is 5>2? ',5>2)
+print('is 5<2? ',5<2)
+
+print('is 5!=2 ',5!=2)
+print('is 5==2 ',5==2)
+print('is 5==5 ',5==5)
+print('is 5>=2 ',5>=2)
+print('is 5<=2 ',5<=2)
+
+print('is 5==5 and 5>2 ',5==5 and 5>2)
+print('is 5==5 and 5<2 ',5==5 and 5<2)
+print('is 5==5 or 5>2 ',5==5 or 5>2)
+print('is 5==5 or 5<2 ',5==5 or 5<2)
+print('is 5==4 or 5<2 ',5==4 or 5<2)
---
+++ capitulo4/inlineif.py
@@ -0,0 +1,10 @@
+# inline if is simpler than if and you use it when you only need to perform
+# a simple task
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+# another example
+x = 10
+y = 20
+if x > y: print("x is greater than y")
